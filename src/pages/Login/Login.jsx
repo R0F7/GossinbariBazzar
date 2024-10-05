20dbd3a0890da6528703f2d2fb01452bcc2c21f1
R0F7 <wwwrafikhan075@gmail.com>
2024-10-05 23:43:18 +0600

    sign Up page design completed

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -4,13 +4,13 @@ import GoogleLogo from '../../assets/google.png';
 
 const Login = () => {
 
-    const handleSubmit =(e)=>{
+    const handleSubmit = (e) => {
         e.preventDefault();
 
         const form = e.target;
         const email = form.email.value;
         const password = form.password.value;
-        console.log(email,password);
+        console.log(email, password);
     }
 
     return (
@@ -42,13 +42,13 @@ const Login = () => {
                                 <input type="password" name="password" id="password" className='border h-[45px] pl-2 rounded-md focus:outline-dotted' placeholder='********' />
                             </label>
                             <div>
-                                <h6 className='px-14 underline pt-1.5 text-[#4A4A4A]'>Forgot password?</h6>
+                                <h6 className='px-14 underline pt-1.5 text-[#4A4A4A] hover:text-[#2E8DD8]'>Forgot password?</h6>
                             </div>
-                            <button type="submit" className="bg-[#006EDC] mx-14 py-2.5 my-6 text-white font-semibold rounded-md transform duration-150 active:scale-95 scale-100">Sign In</button>
+                            <button type="submit" className="cursor-pointer bg-[#2E8DD8] shadow-lg shadow-[rgba(46,141,216,.25)] mx-14 py-2.5 my-6 text-white font-semibold rounded-md transform duration-150 active:scale-95 scale-100">Sign In</button>
                         </form>
                     </div>
                     <div className='text-center py-5 bg-[#F3F8FC] rounded-b-xl'>
-                        <Link to='/signup'><span className='text-[#4A4A4A]'>Not registered?</span> Create an account</Link>
+                        <span className='italic'>Not registered? <Link to='/signup' className='underline hover:text-[#2E8DD8] font-medium'>Create an account</Link></span>
                     </div>
                 </div>
             </div>
@@ -61,7 +61,7 @@ const Login = () => {
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0099ff" fillOpacity="1" d="M0,32L40,26.7C80,21,160,11,240,21.3C320,32,400,64,480,96C560,128,640,160,720,149.3C800,139,880,85,960,64C1040,43,1120,53,1200,74.7C1280,96,1360,128,1400,144L1440,160L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"></path></svg>
             </div>
             <div className='w-full h-full absolute top-[495px] left-0 overflow-hidden -z-50'>
-                <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none" ><path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" className="shape-fill" fill="#4A4A4A" fillOpacity="1"></path></svg>
+                <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none" ><path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" className="shape-fill" fill="#FAE286" fillOpacity="1"></path></svg>
             </div>
         </div>
 
--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -3,103 +3,135 @@ import SignUpImage from '../../assets/Gossinbari SignUp picture 2.jpg';
 import GoogleLogo from '../../assets/google.png';
 import FacebookLogo from '../../assets/facebook.png';
 import Twiter from '../../assets/twiter.png';
+import SignUpBG from '../../assets/gossingbari bazzar signUp bg .jpg';
+import UnknownIMG from '../../assets/unknown Image.jpg';
 
 const SignUp = () => {
     return (
-        <div className='flex justify-center items-center min-h-screen bg-[#F3F8FC] shadow-2xl relative z-50 overflow-hidden '>
-            <div className='w-[70%] h-[750px] border flex bg-[#FBF6F0] gap-10'>
+        <div
+            className='flex justify-center items-center min-h-screen shadow-2xl relative z-50'>
+            {/* BG */}
+            <div className='w-full h-full absolute top-0 left-0 -z-50'><img src={SignUpBG} className='w-full h-full' alt="" /></div>
+
+            <div className='w-[70%] h-[750px] border flex g-[#FBF6F0] bg-[#dee2e6] bg-opacity-70 gap-10'>
                 {/* image */}
                 <div className='w-1/2 h-full relative'>
                     <img className='w-full h-full' src={SignUpImage} alt="" />
                     <div className='absolute top-14 left-12'>
                         {/* TODO react text type */}
+                        {/* Discover the Finest in Food, Fashion & Everyday Essentials. Shop with Ease!" */}
                         <h4 className='text-logo-font-family text-[#006400] text-lg mb-14'>Gossainbari<span className='text-[#4B0082]'>Bazzar</span></h4>
                         <h3 className='text-2xl font-semibold text-[#000080] font-roboto mb-1.5'>Discover the Finest in Food, Fashion &<br></br>Everyday Essentials.</h3>
-                        {/* <p>shop smarter,Live better</p> */}
                         <p className='text-[#4B0082] font-medium'>Shop Smarter with GossainBazzer</p>
-
-                        {/* Discover the Finest in Food, Fashion & Everyday Essentials. Shop with Ease!" */}
+                        {/* <p>shop smarter,Live better</p> */}
                     </div>
                 </div>
 
                 {/* form */}
                 <div className='w-1/2 p-10'>
-                    <p className='text-end p-6 text-[#333333] font-medium italic'>Already a member? <Link to='/login' className='text-[#4B0082] font-bold underline'>Sign In</Link></p>
+                    <p className='text-end p-6 text-[#333333] font-medium italic'>Already a member? <Link to='/login' className='text-[#4B0082] font-bold underline hover:text-[#2E8DD8]'>Sign In</Link></p>
                     <div>
                         <h4 className='mb-10 mt-4 font-roboto text-2xl font-bold text-[#000080]'>Sign Up</h4>
                         <form className='grid grid-cols-8 gap-4'>
-                            <div className='flex flex-col gap-1.5 col-span-4'>
-                                <label htmlFor="name">Username</label>
+
+                            <div className='flex flex-col gap-1 col-span-4'>
+                                <label htmlFor="name" className='font-roboto font-semibold text-[#333333] opacity-95'>Username</label>
                                 <input type="text"
+                                    required
                                     name="name"
                                     id="name"
                                     placeholder='Enter your username'
-                                    className="p-2 shadow rounded-md placeholder:italic placeholder:text-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-2" />
+                                    className="p-2 shadow rounded-md placeholder:italic placeholder:text-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-2 bg-white bg-opacity-40" />
                             </div>
-                            <div className='flex flex-col gap-1.5 col-span-4'>
-                                <label htmlFor="email">Email</label>
+
+                            <div className='flex flex-col gap-1 col-span-4'>
+                                <label htmlFor="email" className='font-roboto font-semibold text-[#333333] opacity-95'>Email</label>
                                 <input
+                                    required
                                     type="email"
                                     name="email"
                                     id="email"
                                     placeholder='Enter your email address'
-                                    className="p-2 shadow rounded-md placeholder:italic placeholder:text-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-2" />
+                                    className="p-2 shadow rounded-md placeholder:italic placeholder:text-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-2 bg-white bg-opacity-40" />
                             </div>
-                            <div className='flex flex-col gap-1.5 col-span-8'>
-                                <label htmlFor="file">Upload File</label>
-                                <input
-                                    type="file"
-                                    name="file"
-                                    id="file"
-                                    className="border p-2" />
+
+                            <div className='col-span-8 flex justify-between items-center'>
+                                <div className='font-roboto w-max text-sm font-medium text-[#333333] opacity-95 border border-[#3992D9] shadow-md shadow-[rgba(0,0,0,.1)] py-2.5 px-10 rounded'>
+                                    <label htmlFor="file" >
+                                        <input
+                                            required
+                                            type="file"
+                                            name="file"
+                                            id="file"
+                                            hidden
+                                            accept='image/*'
+                                            className='text-sm cursor-pointer w-36 hidden' />
+                                        <div className='py-1.5 px-7 cursor-pointer bg-[#2E8DD8] shadow-lg shadow-[rgba(46,141,216,.25)] text-white rounded active:scale-95 scale-100 transform duration-150 hover:shadow-md'>
+                                            Upload Image
+                                        </div>
+                                    </label>
+                                </div>
+                                <div className='w-[70px] border rounded p-2'>
+                                    <img className='w-full' src={UnknownIMG} alt="" />
+                                </div>
                             </div>
-                            <div className='flex flex-col gap-1.5 col-span-4'>
-                                <label htmlFor="password">Password</label>
+
+                            <div className='flex flex-col gap-1 col-span-4'>
+                                <label htmlFor="password" className='font-roboto font-semibold text-[#333333] opacity-95'>Password</label>
                                 <input
+                                    required
                                     type="password"
                                     name="password"
                                     id="password"
                                     placeholder='*******'
-                                    className="p-2 shadow rounded-md placeholder:italic placeholder:text-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-2" />
+                                    className="p-2 shadow rounded-md placeholder:italic placeholder:text-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-2 bg-white bg-opacity-40" />
                             </div>
-                            <div className='flex flex-col gap-1.5 col-span-4'>
-                                <label htmlFor="role">Role</label>
+
+                            <div className='flex flex-col gap-1 col-span-4'>
+                                <label htmlFor="role" className='font-roboto font-semibold text-[#333333] opacity-95'>Role</label>
                                 <select
+                                    required
                                     name="role"
                                     id="role"
-                                    className="p-2 shadow rounded-md placeholder:italic placeholder:text-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-2">
+                                    className="p-2 shadow rounded-md placeholder:italic placeholder:text-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-2 bg-white bg-opacity-40">
                                     <option value="" disabled selected className=''>role</option>
                                     <option value="user">user</option>
                                     <option value="seller">seller</option>
                                 </select>
                             </div>
-                            <div className='flex str col-span-8 gap-2.5'>
+
+                            <div className='flex col-span-8 gap-2.5'>
                                 <input
+                                    required
                                     type="checkbox"
                                     name="check"
                                     id="check"
-                                    className='relative bottom-2.5 text-xl' />
+                                    className='relative bottom-1.5 text-xl w-6 ' />
                                 <p htmlFor="check" className='italic text-sm'>Creating an account means you're okay with our <span className='text-[#4B0082] font-semibold'>Terms of Service, Privacy Policy, </span>and our <span className='text-[#4B0082] font-semibold'>default Notification Settings</span></p>
                             </div>
+
                             <div className='col-span-8 flex items-center justify-between mt-2.5'>
-                                <input type="submit" className=' bg-blue-500 text-white p-2.5 px-20' value="Create account" />
+                                <input
+                                    type="submit"
+                                    className=' font-medium bg-[#2E8DD8] shadow-lg shadow-[rgba(46,141,216,.25)] text-white p-2.5 px-20 rounded-md transform duration-150 active:scale-95 scale-100 hover:shadow-md' value="Create account" />
                                 <h4 className='font-bold text-2xl text-[#4B0082]'>OR</h4>
-                                <h6 className='text-[#4B0082] font-medium'>Use the platforms below</h6>
+                                <h6 className='text-[#4B0082] font-medium pr-4'>Use the platforms below</h6>
                             </div>
+
                         </form>
 
                         <div className='w-full h-[1.5px] bg-[#4B0082] my-8'></div>
 
                         <div>
-                            <div className='flex gap-10'>
-                                <button className='flex items-center gap-2 bg-white shadow-md py-2 px-6 font-semibold'>
+                            <div className='flex gap-7'>
+                                <button className='flex items-center gap-2 bg-white shadow-md shadow-[rgba(0,0,0,.2)] py-2 px-6 font-semibold rounded active:scale-95 scale-100 transform duration-150 hover:shadow-md'>
                                     <img className='w-5' src={GoogleLogo} alt="" />
                                     <h4>Sign Up with Google</h4>
                                 </button>
-                                <button className='flex items-center gap-2 bg-white shadow-md py-2 px-4 font-semibold'>
+                                <button className='flex items-center gap-2 bg-white shadow-md shadow-[rgba(0,0,0,.23)] py-2 px-3 font-semibold rounded active:scale-95 scale-100 transform duration-150 hover:shadow-md'>
                                     <img className='w-5' src={FacebookLogo} alt="" />
                                 </button>
-                                <button className='flex items-center gap-2 bg-white shadow-md py-2 px-4 font-semibold'>
+                                <button className='flex items-center gap-2 bg-white shadow-md shadow-[rgba(0,0,0,.23)]  py-2 px-3 font-semibold rounded active:scale-95 scale-100 transform duration-150 hover:shadow-md'>
                                     <img className='w-5' src={Twiter} alt="" />
                                 </button>
                             </div>
