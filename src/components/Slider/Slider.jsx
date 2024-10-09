a34a438d379e4ff6337b5cb51d4cbb673411577b
R0F7 <wwwrafikhan075@gmail.com>
2024-10-10 00:42:09 +0600

    add slider but have some bug

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -3,27 +3,13 @@ import Clothing from "../../assets/slider/Clothing & Apparel.webp";
 import juwelier from "../../assets/slider/juwelier producy.jpg";
 import Furniture from "../../assets/slider/Furniture & Home Decor.jpg";
 import "../Slider/Slider.css";
+import { useEffect } from "react";
+import sliderUtilityFunction from "./sliderUtilityFunction";
 
 const Slider = () => {
-  let nextDom = document.getElementById("next");
-  let prevDom = document.getElementById("prev");
-  let carouselDom = document.querySelector(".carousel");
-  let listItemDom = document.querySelector(".carousel .list");
-  let thumbnailDom = document.querySelector(".carousel .thumbnail");
-
-  function showSlider(type) {
-    let itemSlider = document.querySelectorAll(".carousel .list .item");
-    let itemThumbnail = document.querySelectorAll(".carousel .thumbnail .item");
-
-    if (type === "next") {
-      listItemDom.appendChild(itemSlider[0]);
-      thumbnailDom.appendChild(itemThumbnail[0]);
-      carouselDom.classList.add("next");
-    }
-  }
-//   nextDom.onclick = function () {
-//     showSlider("next");
-//   };
+  useEffect(() => {
+    sliderUtilityFunction();
+  }, []);
 
   return (
     <>
@@ -138,8 +124,8 @@ const Slider = () => {
         </div>
         {/* arrows */}
         <div className="arrows">
+          <button id="next">&lt;</button> {/* < symbol */}
           <button id="prev">&gt;</button> {/* > symbol */}
-          <button id="next" onClick={()=> showSlider('next')}>&lt;</button> {/* < symbol */}
         </div>
         <div className="time"></div>
       </div>
