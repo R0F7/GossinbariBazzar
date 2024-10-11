b3ed3924cbb610d2788cf6e7dc08c6c9133263da
R0F7 <wwwrafikhan075@gmail.com>
2024-10-11 16:56:33 +0600

    footer style complete

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,16 +1,20 @@
 import Logo from "../../assets/Gossaigbari_Bazzar_logo_crop-removebg-preview.png";
 import { FaFacebookF, FaLinkedinIn } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
-import { IoHomeOutline, IoLogoInstagram } from "react-icons/io5";
+import {
+  IoHomeOutline,
+  IoLocationOutline,
+  IoLogoInstagram,
+} from "react-icons/io5";
 import { LuPhone } from "react-icons/lu";
 import { TfiEmail } from "react-icons/tfi";
 
 const Footer = () => {
   return (
-    <footer>
-      <div>
+    <footer className="container mx-auto pt-12">
+      <div className="grid grid-cols-6 gap-10">
         {/* BIO */}
-        <div>
+        <div className="col-span-2">
           {/* logo+Name  */}
           <div className="flex items-center gap-1">
             <img className="w-12 rop-shadow-2xl" src={Logo} alt="" />
@@ -18,28 +22,37 @@ const Footer = () => {
               Gossainbari<span className="text-[#4B0082]">Bazzar</span>
             </h4>
           </div>
-          <p>
+          <p className="py-6 w-[85%] text-[#4E565F] font-medium">
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio earum
             quidem cum dolorem deleniti saepe.
           </p>
-          <div>
-            <ul>
-              <li>
-                <i><TfiEmail /></i>
+          <div className="pb-8">
+            <ul className="space-y-2.5 text-[#4E565F]">
+              <li className="flex items-center gap-2">
+                <i>
+                  <TfiEmail />
+                </i>
                 <h4>contact@example.com</h4>
               </li>
-              <li>
-                <i><LuPhone /></i>
-                <h4>016 12500106</h4>
+
+              <li className="flex items-center gap-2">
+                <i>
+                  <LuPhone />
+                </i>
+                <h4 className="font-bold text-[#212B36]">016 12500106</h4>
               </li>
-              <li>
-                <i><IoHomeOutline /></i>
+
+              <li className="flex items-center gap-2">
+                <i>
+                  <IoHomeOutline />
+                </i>
+                {/* <i><IoLocationOutline /></i> */}
                 <h4>Gossainbari Bogura, Bangladesh</h4>
               </li>
             </ul>
           </div>
           <div>
-            <ul>
+            <ul className="flex items-center gap-6 pb-8 text-[#212B36] text-[17px]">
               <li>
                 <FaFacebookF />
               </li>
@@ -59,8 +72,8 @@ const Footer = () => {
 
         {/* ABOUT */}
         <div>
-          <h4>ABOUT</h4>
-          <ul>
+          <h4 className="text-[#212B36] mb-3.5 font-bold">ABOUT</h4>
+          <ul className="space-y-3 text-[#4E565F] text-[15px] font-medium">
             <li>About Us</li>
             <li>Contact Us</li>
             <li>Help Center</li>
@@ -70,8 +83,8 @@ const Footer = () => {
 
         {/* HELP & GUIDE */}
         <div>
-          <h4>HELP & GUIDE</h4>
-          <ul>
+          <h4 className="text-[#212B36] mb-3.5 font-bold">HELP & GUIDE</h4>
+          <ul className="space-y-3 text-[#4E565F] text-[15px] font-medium">
             <li>Term Of Use</li>
             <li>Privacy Policy</li>
             <li>Shipping & Delivery</li>
@@ -79,25 +92,38 @@ const Footer = () => {
         </div>
 
         {/* NEWSLETTER */}
-        <div>
-          <h4>NEWSLETTER</h4>
+        <div className="col-span-2">
+          <h4 className="text-[#212B36] mb-3.5 font-bold">NEWSLETTER</h4>
           <div>
-            <p>
+            <p className="text-[#4E565F] w-[50%] text-[15px]">
               Don’t miss out <strong>thousands of great deals</strong> &
               promotions.
             </p>
-            <form>
-              <input type="email" name="" id="" />
-              <button type="submit">Subscribe</button>
+            <form className="flex flex-col">
+              <input
+                type="email"
+                name=""
+                id=""
+                className="bg-[#F4F6F8] border outline-none p-2 w-[60%] rounded-md shadow-sm my-4"
+                placeholder="Email address..."
+              />
+              <button
+                type="submit"
+                className="bg-[rgb(46,141,216)] text-white w-[115px] border-none py-2 rounded-md font-semibold shadow-sm transform duration-150 active:scale-95 scale-100"
+              >
+                Subscribe
+              </button>
             </form>
           </div>
         </div>
       </div>
       {/* footer - footer */}
-      <div>
-        <h6>© 2024 GossainbaiBazzar. All rights reserved by R0F7.</h6>
+      <div className="py-6 border-t text-center">
+        <h6 className="text-[#4E565F]">
+          © 2024 <strong>GossainbaiBazzar.</strong> All rights reserved by R0F7.
+        </h6>
         {/* payment method logo */}
-        <div></div>
+        {/* <div></div> */}
       </div>
     </footer>
   );
