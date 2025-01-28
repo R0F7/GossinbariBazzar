ca625f6aab3444750a034f8bd3e0180583c9dcff
R0F7 <wwwrafikhan075@gmail.com>
2025-01-29 01:09:22 +0600

    update profile

--- src/pages/Dashboard/MyAccount/Profile/Profile.jsx
+++ src/pages/Dashboard/MyAccount/Profile/Profile.jsx
@@ -1,44 +1,90 @@
+import { useState } from "react";
 import unknownImage from "../../../../assets/unknown Image.jpg";
 import useAuth from "../../../../hooks/useAuth";
+import { TbEditOff, TbPhotoEdit } from "react-icons/tb";
 
 const Profile = () => {
   const { user, user_info_DB } = useAuth();
-  console.log(user_info_DB);
+  const [focusedField, setFocusedField] = useState(null);
+  const [toggle, setToggle] = useState(false);
+//   console.log(user?.providerData[0]?.uid);
+
+  const handleFocus = (field) => {
+    setFocusedField(field);
+  };
+
+  const handleBlur = () => {
+    setFocusedField(null);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const number = form.number.value;
+    const date_of_birth = form.DB.value;
+    const address = form.address.value;
+    const about = form.about.value;
+
+    console.table(name, email, number, date_of_birth, address, about);
+  };
 
   return (
-    <div className="border flex justify-center h-[98%] items-center bg-gradient-to-r from-[#2E8DD8] via-[#FFD700] to-[#4B0082]">
-      <div className="border w-full bg-white bg-opacity-75 p-4 m-4 rounded-md shadow flex justify-center">
+    <div className="border flex justify-center h-[97%] items-center bg-gradient-to-r from-[#2E8DD8] via-[#FFD700] to-[#4B0082]">
+      <div className="border w-full g-white g-opacity-75 glass-bg py-9 m-[94px] rounded-md shadow flex justify-center">
         <div className="w-ful">
           <div className="">
-            <h4 className="text-slate-500 font-semibold text-lg mb-3">
+            {/* <h4 className="text-slate-800 font-bold text-lg mb-3">
               Profile picture
-            </h4>
+            </h4> */}
             <div className="flex items-center gap-4">
               {/* image part */}
-              <div className="w-24 h-24 border-2 rounded-xl">
+              <div className="w-24 h-24 rounded-full p-[5px] bg-gradient-to-r from-[#3A92CC] to-[#feb47b]">
                 <img
-                  className="w-full h-full rounded-xl"
+                  className="w-full h-full object-cover rounded-full"
                   src={user?.photoURL || unknownImage}
-                  alt=""
+                  alt="User Avatar"
                 />
               </div>
 
-              <div>
-                <h3 className="text-lg font-bold">
-                  {user?.displayName} <span>({user_info_DB?.role})</span>
-                </h3>
-                <h3 className="text-slate-600">{user?.email}</h3>
-              </div>
+              <div className="flex gap-">
+                <div>
+                  <h3 className="text-lg font-bold">
+                    {user?.displayName}{" "}
+                    <span className="text-[#4F0480] text-sm">
+                      ({user_info_DB?.role})
+                    </span>
+                  </h3>
+                  <h4 className="text-slate-600 font-bold">UID : <span className="font-semibold">{user?.providerData[0]?.uid}</span></h4>
+                  <h4 className="text-slate-600">{user?.email}</h4>
+                </div>
 
-              {/* buttons */}
-              {/* <div className="flex flex-col ml-3.5 gap-3">
-              <button className="upload-button h-[45px] w-[140px] shadow hover:shadow-md active:scale-95 scale-100 transition duration-300">
-                Upload now
-              </button>
-              <button className="remove-button h-[45px] w-[140px] shadow hover:shadow-md active:scale-95 scale-100 transition-transform duration-300">
-                Remove
-              </button>
-            </div> */}
+                <div
+                  onClick={() => setToggle(!toggle)}
+                  className="w-10 h-10 p-2 ml-2 rounded-full hover:bg-white hover:bg-opacity-30 transition duration-500 flex items-center justify-center text-xl"
+                >
+                  {toggle ? (
+                    <TbEditOff title="Cancel Edit" />
+                  ) : (
+                    <TbPhotoEdit title="Edit Image" />
+                  )}
+                </div>
+
+                {/* buttons */}
+                <div
+                  className={`flex flex-col gap-3 border-l-2 border-[#540B7B] pl-6 ml-6 ${
+                    toggle ? "scale-y-100" : "scale-y-0"
+                  } transition duration-300 origin-top`}
+                >
+                  <button className="upload-button h-[35px] w-[120px] text-sm shadow hover:shadow-md active:scale-95 scale-100 transition duration-300">
+                    Upload now
+                  </button>
+                  <button className="remove-button h-[35px] w-[120px] text-sm shadow hover:shadow-md active:scale-95 scale-100 transition-transform duration-300">
+                    Remove
+                  </button>
+                </div>
+              </div>
             </div>
 
             {/* <span className="w-[2px] bg-[#2E8DD8] mx-10"></span> */}
@@ -60,8 +106,11 @@ const Profile = () => {
 
           {/* form part */}
           <div className="-[65%] mt-10 x-auto">
-            <form className="grid grid-cols-2 gap-x-6 gap-y-3">
-              <label htmlFor="name" className="flex flex-col gap-2">
+            <form
+              className="grid grid-cols-2 gap-x-6 gap-y-3"
+              onSubmit={handleSubmit}
+            >
+              <label htmlFor="name" className="flex flex-col gap-2 relative">
                 <span className="text-slate-500 font-semibold">Full Name</span>
                 <input
                   type="text"
@@ -69,10 +118,19 @@ const Profile = () => {
                   id="name"
                   defaultValue={user?.displayName}
                   placeholder="Jane Doe"
-                  className="border-2 border-slate-400 p-2.5  outline-none rounded-md focus:border-[#2E8DD8] text-slate-600 "
+                  className="border-2 border-[#3B91CA] p-2.5 outline-none rounded-md focus:border-[#2E8DD8] focus:border-2 focus:border-l-[#530A7C] focus:border-t-[#530A7C] text-slate-600 bg-transparent border-b-2 border-t-transparent border-l-transparent transition duration-1000 shadow-lg focus:shadow-xl placeholder:font-semibold font-semibold placeholder:text-slate-500"
+                  onFocus={() => handleFocus("name")}
+                  onBlur={handleBlur}
                 />
+                <span
+                  className={`text-xs font-semibold text-slate-700 border border-[#530A7C] w-[100px] text-center py-0.5 rounded-full shadow-md absolute bottom-9 right-9 bg-[#D6C759] ${
+                    focusedField === "name" ? "scale-100" : "scale-0"
+                  } transition duration-1000`}
+                >
+                  Full Name
+                </span>
               </label>
-              <label htmlFor="email" className="flex flex-col gap-2">
+              <label htmlFor="email" className="flex flex-col gap-2 relative">
                 <span className="text-slate-500 font-semibold">
                   Email Address
                 </span>
@@ -82,10 +140,19 @@ const Profile = () => {
                   id="email"
                   defaultValue={user?.email}
                   placeholder="Jane@Doe.com"
-                  className="border-2 border-slate-400 p-2.5  outline-none rounded-md focus:border-[#2E8DD8] text-slate-600 "
+                  className="border-2 border-[#3B91CA] p-2.5  outline-none rounded-md focus:border-[#2E8DD8] focus:border-2 focus:border-l-[#530A7C] focus:border-t-[#530A7C] text-slate-600 bg-transparent border-b-2 border-t-transparent border-l-transparent transition duration-1000 shadow-lg focus:shadow-xl placeholder:font-semibold font-semibold placeholder:text-slate-500"
+                  onFocus={() => handleFocus("email")}
+                  onBlur={handleBlur}
                 />
+                <span
+                  className={`text-xs font-semibold text-slate-700 border border-[#530A7C] w-[100px] text-center py-0.5 rounded-full shadow-md absolute bottom-9 right-9 bg-[#C19761] ${
+                    focusedField === "email" ? "scale-100" : "scale-0"
+                  } transition duration-1000`}
+                >
+                  Email Address
+                </span>
               </label>
-              <label htmlFor="number" className="flex flex-col gap-2">
+              <label htmlFor="number" className="flex flex-col gap-2 relative">
                 <span className="text-slate-500 font-semibold">
                   Phone Number
                 </span>
@@ -95,10 +162,19 @@ const Profile = () => {
                   id="number"
                   defaultValue={user_info_DB?.number}
                   placeholder="0123456789"
-                  className="border-2 border-slate-400 p-2.5  outline-none rounded-md focus:border-[#2E8DD8] text-slate-600 "
+                  className="border-2 border-[#3B91CA] p-2.5  outline-none rounded-md focus:border-[#2E8DD8] focus:border-2 focus:border-l-[#530A7C] focus:border-t-[#530A7C] text-slate-600 bg-transparent border-b-2 border-t-transparent border-l-transparent transition duration-1000 shadow-lg focus:shadow-xl placeholder:font-semibold font-semibold placeholder:text-slate-500"
+                  onFocus={() => handleFocus("number")}
+                  onBlur={handleBlur}
                 />
+                <span
+                  className={`text-xs font-semibold text-slate-700 border border-[#530A7C] w-[100px] text-center py-0.5 rounded-full shadow-md absolute bottom-[38px] right-9 bg-[#D4C95C] ${
+                    focusedField === "number" ? "scale-100" : "scale-0"
+                  } transition duration-1000`}
+                >
+                  Number
+                </span>
               </label>
-              <label htmlFor="DB" className="flex flex-col gap-2">
+              <label htmlFor="DB" className="flex flex-col gap-2 relative">
                 <span className="text-slate-500 font-semibold">
                   Date of Birth
                 </span>
@@ -106,12 +182,21 @@ const Profile = () => {
                   type="date"
                   name="DB"
                   id="DB"
-                  className="border-2 border-slate-400 p-2.5  outline-none rounded-md focus:border-[#2E8DD8] text-slate-600 "
+                  className="border-2 border-[#3B91CA] p-2.5  outline-none rounded-md focus:border-[#2E8DD8] focus:border-2 focus:border-l-[#530A7C] focus:border-t-[#530A7C] text-slate-600 bg-transparent border-b-2 border-t-transparent border-l-transparent transition duration-1000 shadow-lg focus:shadow-xl placeholder:font-semibold font-semibold placeholder:text-slate-500"
+                  onFocus={() => handleFocus("date-of-birth")}
+                  onBlur={handleBlur}
                 />
+                <span
+                  className={`text-xs font-semibold text-slate-700 border border-[#530A7C] w-[100px] text-center py-0.5 rounded-full shadow-md absolute bottom-[38px] right-9 bg-[#C19860] ${
+                    focusedField === "date-of-birth" ? "scale-100" : "scale-0"
+                  } transition duration-1000`}
+                >
+                  Date of Birth
+                </span>
               </label>
               <label
                 htmlFor="address"
-                className="flex flex-col gap-2 col-span-2"
+                className="flex flex-col gap-2 col-span-2 relative"
               >
                 <span className="text-slate-500 font-semibold">Address</span>
                 <input
@@ -120,17 +205,38 @@ const Profile = () => {
                   id="address"
                   defaultValue={user_info_DB?.address}
                   placeholder="Jane Doe"
-                  className="border-2 border-slate-400 p-2.5  outline-none rounded-md focus:border-[#2E8DD8] text-slate-600 "
+                  className="border-2 border-[#3B91CA] p-2.5  outline-none rounded-md focus:border-[#2E8DD8] focus:border-2 focus:border-l-[#530A7C] focus:border-t-[#530A7C] text-slate-600 bg-transparent border-b-2 border-t-transparent border-l-transparent transition duration-1000 shadow-lg focus:shadow-xl placeholder:font-semibold font-semibold placeholder:text-slate-500"
+                  onFocus={() => handleFocus("address")}
+                  onBlur={handleBlur}
                 />
+                <span
+                  className={`text-xs font-semibold text-slate-700 border border-[#530A7C] w-[100px] text-center py-0.5 rounded-full shadow-md absolute bottom-9 right-9 bg-[#C19860] ${
+                    focusedField === "address" ? "scale-100" : "scale-0"
+                  } transition duration-1000`}
+                >
+                  Address
+                </span>
               </label>
-              <label htmlFor="about" className="flex flex-col gap-2 col-span-2">
+              <label
+                htmlFor="about"
+                className="flex flex-col gap-2 col-span-2 relative"
+              >
                 <span className="text-slate-500 font-semibold">About</span>
                 <textarea
                   name="about"
                   id="about"
-                  placeholder="About"
-                  className="border-2 border-slate-400 p-2.5 h-[120px] outline-none rounded-md focus:border-[#2E8DD8] focus:border- text-slate-600 "
+                  placeholder="Write a quirky intro or a heartfelt description..."
+                  className="border-2 border-[#3B91CA] p-2.5 h-[120px] outline-none rounded-md focus:border-[#2E8DD8] focus:border-2 focus:border-l-[#530A7C] focus:border-t-[#530A7C] text-slate-600 bg-transparent border-b-2 border-t-transparent border-l-transparent transition duration-1000 shadow-lg focus:shadow-xl placeholder:font-semibold font-semibold placeholder:text-slate-500"
+                  onFocus={() => handleFocus("about")}
+                  onBlur={handleBlur}
                 ></textarea>
+                <span
+                  className={`text-xs font-semibold text-slate-700 border border-[#530A7C] w-[100px] text-center py-0.5 rounded-full shadow-md absolute bottom-[115px] right-9 bg-[#C19860] ${
+                    focusedField === "about" ? "scale-100" : "scale-0"
+                  } transition duration-1000`}
+                >
+                  About
+                </span>
               </label>
               <div>
                 <button
