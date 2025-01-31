c6b9c769cb97684a2bd2286aafb1e70a5dd6926d
R0F7 <wwwrafikhan075@gmail.com>
2025-02-01 00:15:52 +0600

    update saved address ui

--- src/pages/Dashboard/MyAccount/SavedAddress/SavedAddress.jsx
+++ src/pages/Dashboard/MyAccount/SavedAddress/SavedAddress.jsx
@@ -3,9 +3,9 @@ import unknownImage from "../../../../assets/unknown Image.jpg";
 
 const SavedAddress = () => {
   return (
-    <div className="min-h-screen w-full bg-slate-800 g-opacity-85 g-[#F4EDD3]">
+    <div className="min-h-screen w-full g-slate-800 g-opacity-85 bg-[} chic-bg">
       <div className="w-[54%] mx-auto pt-20 relative overflow-hidden rounded-lg">
-        <div className="ice-bg text-slate-500  rounded-lg shadow-lg relative pb-3 overflow-hidden">
+        <div className="ice-bg text-slate-500  rounded-lg shadow-lg relative pb-3 overflow-hidden mt-8">
           <div className="flex gap-6 py-10 pl-10">
             {/* circle */}
             <div className="relative">
@@ -19,67 +19,67 @@ const SavedAddress = () => {
                 </div>
               </div>
               <div className="absolute top-0 left-0 -z-50">
-                <div className="bg-[#4B0082] w-[200px] h-[600px] -translate-y-[348px] -translate-x-[35px] rotate-45"></div>
+                <div className="bg-[#0077B6] w-[200px] h-[600px] -translate-y-[348px] -translate-x-[35px] rotate-45"></div>
               </div>
             </div>
 
             <div className="mt-24">
-              <h2 className="text-slate-900 font-bold text-3xl textshado underline decoration-wavy pb-6">
+              <h2 className="ext-slate-900 t ext-[#FF6B6B] t ext-[#00D1FF] text-[#FFD700] font-bold text-3xl textshado underline decoration-wavy pb-6">
                 Jonathan Smith.
               </h2>
 
               {/* info field */}
               <div className="grid grid-cols-2  gap-y-2 gap-x-14 mr-6">
-                <div className="text-slate-950 flex gap-1.5 order-b pb-1 shadow-2xl">
+                <div className="text-slate-700 flex gap-1.5 order-b pb-1 shadow-2xl">
                   <i>
-                    <FaLocationDot className="w-4 h-4 mt-[5px]" />
+                    <FaLocationDot className="w-4 h-4 mt-[5px] text-[#0077B6] ext-opacity-90" />
                   </i>
                   <h4 className="flex flex-col">
-                    <span className="font-bold underline decoration-double">
-                      8759 Jones Lane
+                    <span className="font-bold nderline ecoration-double">
+                      87594545454
                     </span>
                   </h4>
                 </div>
 
-                <div className="text-slate-950 flex gap-1.5">
+                <div className="text-slate-700 flex gap-1.5">
                   <i>
-                    <FaLocationDot className="w-4 h-4 mt-[5px]" />
+                    <FaLocationDot className="w-4 h-4 mt-[5px] text-[#0077B6]" />
                   </i>
                   <h4 className="flex flex-col">
-                    <span className="font-medium">8759 Jones Lane</span>
+                    <span className="font-bold">8759 Jones Lane</span>
                   </h4>
                 </div>
 
-                <div className="text-slate-950 flex gap-1.5">
+                <div className="text-slate-700 flex gap-1.5">
                   <i>
-                    <FaLocationDot className="w-4 h-4 mt-[5px]" />
-                  </i>
-                  <h4 className="flex flex-col">
-                    <span className="font-medium">8759 Jones Lane </span>
-                  </h4>
-                </div>
-
-                <div className="text-slate-950 flex gap-1.5">
-                  <i>
-                    <FaLocationDot className="w-4 h-4 mt-[5px]" />
-                  </i>
-                  <h4 className="flex flex-col">
-                    <span className="font-medium">8759 Jones Lane </span>
-                  </h4>
-                </div>
-
-                <div className="text-slate-900 flex gap-1.5">
-                  <i>
-                    <FaLocationDot className="w-4 h-4 mt-[5px]" />
+                    <FaLocationDot className="w-4 h-4 mt-[5px] text-[#0077B6]" />
                   </i>
                   <h4 className="flex flex-col">
                     <span className="font-bold">8759 Jones Lane </span>
                   </h4>
                 </div>
 
-                <div className="text-slate-900 flex gap-1.5">
+                <div className="text-slate-700 flex gap-1.5">
                   <i>
-                    <FaLocationDot className="w-4 h-4 mt-[5px]" />
+                    <FaLocationDot className="w-4 h-4 mt-[5px] text-[#0077B6]" />
+                  </i>
+                  <h4 className="flex flex-col">
+                    <span className="font-bold">8759 Jones Lane </span>
+                  </h4>
+                </div>
+
+                <div className="text-slate-700 flex gap-1.5">
+                  <i>
+                    <FaLocationDot className="w-4 h-4 mt-[5px] text-[#0077B6]" />
+                  </i>
+                  <h4 className="flex flex-col">
+                    <span className="font-bold">8759 Jones Lane </span>
+                  </h4>
+                </div>
+
+                <div className="text-slate-700 flex gap-1.5">
+                  <i>
+                    <FaLocationDot className="w-4 h-4 mt-[5px] text-[#0077B6]" />
                   </i>
                   <h4 className="flex flex-col">
                     <span className="font-bold flex">
@@ -95,7 +95,7 @@ const SavedAddress = () => {
           <div className="absolute top-0 left-0">
             <div className="bg-[#2E8DD8] w-36 h-7 translate-y-[204px] -translate-x-[30px] -rotate-45"></div>
           </div>
-          <div className="bg-[#2E8DD8] w-[150px] h-[150px] absolute top-0 -left-0 -translate-x-20 -translate-y-[114px] rotate-45"></div>
+          <div className="bg-[#2E8DD8] w-[150px] h-[150px] absolute top-0 -left-0 -translate-x-20 -translate-y-[114px] rotate-45 shadow-md border border-slate-200 border-opacity-45"></div>
         </div>
 
         <div className="absolute -bottom-[1px] -right-[125px] -rotate-45">
